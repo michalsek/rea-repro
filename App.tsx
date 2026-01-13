d60675692d4dcfcdf6cee8418f264eeb4722626f
Michał Sęk <michal.sek@swmansion.com>
2026-01-13 14:07:38 +0100

    init

--- App.tsx
+++ App.tsx
@@ -1,7 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useSharedValue } from "react-native-reanimated";
+
+function someInternalWorklet<T extends number[]>(s: T): T {
+  "worklet";
+
+  return s.map((v) => v * 2) as T;
+}
 
 export default function App() {
+  const arraySV = useSharedValue<number[]>([]);
+
+  useEffect(() => {
+    arraySV.modify((arr) => {
+      "worklet";
+
+      arr.push(Math.random());
+
+      // using number[] directly causes the function to warn of TS error
+      return someInternalWorklet<number[]>(arr);
+    });
+
+    arraySV.modify((arr) => {
+      "worklet";
+
+      arr.push(Math.random());
+
+      // If internal function is generic and can infer <T extends Value> (here T = number[]),
+      // then no TS error is raised
+      return someInternalWorklet(arr);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
